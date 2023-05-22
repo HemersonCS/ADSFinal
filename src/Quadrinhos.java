5ea14ec17655e26dac880006781c4c2933f36c43
Hemerson Cordeiro <hemerson.hcs@gmail.com>
2023-05-21 21:13:47 -0300

    Reenvio criada classe Quadrinhos

---
+++ src/Quadrinhos.java
@@ -0,0 +1,15 @@
+public class Quadrinhos {
+
+	private String titulo;
+    private int anoLancamento;
+    private String ilustrador;
+	private int quantidadeFasciculos;
+	
+	public Quadrinhos(String titulo, int anoLancamento, String ilustrador, int quantidadeFasciculos) {
+		this.titulo = titulo;
+        this.anoLancamento = anoLancamento;
+		this.ilustrador = ilustrador;
+		this.quantidadeFasciculos = quantidadeFasciculos;
+	}
+	
+}
