c61a4ef53e3f6aa4c71d54fbde0986002dedd85b
Hemerson Cordeiro <hemerson.hcs@gmail.com>
2023-05-29 18:23:03 -0300

    criados metodos na Classe Quadrinhos

--- src/Quadrinhos.java
+++ src/Quadrinhos.java
@@ -1,15 +1,36 @@
+import java.util.ArrayList;
+
 public class Quadrinhos {
 
 	private String titulo;
     private int anoLancamento;
     private String ilustrador;
-	private int quantidadeFasciculos;
 	
-	public Quadrinhos(String titulo, int anoLancamento, String ilustrador, int quantidadeFasciculos) {
+	public Quadrinhos(String titulo, int anoLancamento, String ilustrador) {
 		this.titulo = titulo;
         this.anoLancamento = anoLancamento;
 		this.ilustrador = ilustrador;
-		this.quantidadeFasciculos = quantidadeFasciculos;
+	}
+
+	ArrayList<Quadrinhos> quadrinhos = new ArrayList<Quadrinhos>();
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public int getAnoLancamento() {
+		return anoLancamento;
+	}
+
+	public String getIlustrador() {
+		return ilustrador;
+	}
+
+	public void cadastrar () {
+		quadrinhos.add(null);
 	}
 	
+	public void exibir () {
+		System.out.println ("Nome: "+ getTitulo()+ ", foi lançado em "+getAnoLancamento()+", ilustrado por "+getIlustrador());
+		}
 }
