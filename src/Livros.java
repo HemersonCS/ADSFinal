5a24148656f92492b6fd354d39be16764a330934
EriTXS <133248197+EriTXS@users.noreply.github.com>
2023-05-31 20:03:44 -0300

    Criada class Livross

---
+++ src/Livros.java
@@ -0,0 +1,48 @@
+public class Livros {
+
+	private String titulo;
+	private int anoLancamento;
+	private String escritor;
+	private int quantidadePaginas;
+	
+	public Livros(String titulo, int anoLancamento, String escritor, int quantidadePaginas) {
+		this.titulo = titulo;
+		this.anoLancamento = anoLancamento;
+		this.escritor = escritor;
+		this.quantidadePaginas = quantidadePaginas;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getAnoLancamento() {
+		return anoLancamento;
+	}
+
+	public void setAnoLancamento(int anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+
+	public String getEscritor() {
+		return escritor;
+	}
+
+	public void setEscritor(String escritor) {
+		this.escritor = escritor;
+	}
+
+	public int getQuantidadePaginas() {
+		return quantidadePaginas;
+	}
+
+	public void setQuantidadePaginas(int quantidadePaginas) {
+		this.quantidadePaginas = quantidadePaginas;
+	}
+
+		
+}
