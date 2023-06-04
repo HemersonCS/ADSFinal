d2475474769e5a14bca66bdb2b544990d09f99b9
isabel1971carvalho@gmail.com <isabel1971carvalho@gmail.com>
2023-06-03 21:11:39 -0300

    Chamando método da classe filme

--- src/Series.java
+++ src/Series.java
@@ -47,10 +47,18 @@ public class Series {
 	public void exibeFichaTecnica(){
 		System.out.println("Nome do filme: " + nome);
 		System.out.println("Nome do criador: " + criador);
-		System.out.println("Nome do criador: " + quantidadeEpisodios);
+		System.out.println("Quantidade de episódio: " + quantidadeEpisodios);
 	}
 	public double exibeMediaAvaliacao() {
 		return somaDasAvaliacoes / totalDeAvaliacoes;
 	}
-	
+//Chamada de método da classe Filme
+	Filme filmes = new Filme(criador, false, totalDeAvaliacoes);
+	public void FoiAssitido(){
+
+	}
+//Criando método na classe Filme
+	public void ExibeTituloFilme(){
+		filmes.getTitulo_Filme();
+	}
 }
